4decd6445314f1f76c74f11396ac758744b8aef2
ffreijefuente <ffreijefuente@gmail.com>
2023-01-27 18:46:41 +0100

    Add swagger doc

--- metric-crud-app-backend/src/app.ts
+++ metric-crud-app-backend/src/app.ts
@@ -2,7 +2,9 @@
 import express, { type Request, type Response, type NextFunction, type Application } from 'express'
 import metricRouter from './routes/MetricRouter'
 import morgan from 'morgan'
+import swaggerUi from 'swagger-ui-express'
 import { ValidationError } from 'express-json-validator-middleware'
+import swaggerFile from '../swagger.json'
 
 const app: Application = express()
 
@@ -10,6 +12,11 @@ app.use(morgan('tiny'))
 app.use(express.json())
 
 app.use('/metric', metricRouter)
+app.use(
+  '/',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerFile)
+)
 
 app.use((error: Error, req: Request, response: Response, next: NextFunction) => {
   if (error instanceof ValidationError) {
